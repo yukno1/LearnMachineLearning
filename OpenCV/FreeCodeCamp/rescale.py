ac7f4bb572e4dd74b1fefb2edec164469038e932
yukno1 <121278340+yukno1@users.noreply.github.com>
2025-08-25 15:00:54 +0800

    2025.8.25.15:00; opencv

---
+++ OpenCV/FreeCodeCamp/rescale.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+
+
+def rescaleFrame(frame, scale=0.75):
+    # img, video, live video
+
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = cv.imread('./Resources/Photos/cat.jpg')
+cv.imshow('Original', img)
+
+img_resized = rescaleFrame(img)
+cv.imshow('Resized', img_resized)
+cv.waitKey(0)
+
+capture = cv.VideoCapture('./Resources/Videos/dog.mp4')
+
+
+
+# another way of rescaling video specifically, using capture.set()
+def changeRes(width, height):
+    # only for live video (from external camera, webcam)
+
+    capture.set(3, width)
+    capture.set(4, height)
+
+
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Resized', frame_resized)
+
+    if cv.waitKey(20) and 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
--- OpenCV/murtaza/in 3 hrs/ch1.py
+++ OpenCV/murtaza/in 3 hrs/ch1.py
@@ -1,4 +1,4 @@
 import cv2
 
 # read img
-img = cv2.imread()
+img = cv2.imread("Resources/lena.png")
